3777d11a3b6f91c62f469a71c613b3e5f8f2e210
yagofontanez <yagofontanez20@icloud.com>
2024-09-07 18:07:26 -0300

    feat: liberando cors

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const routes = require('./routes');
 const app = express();
 const cors = require('cors');
 
+app.use(cors);
 app.use(express.json());
 app.use(routes);
-app.use(cors);
 
 const { Pool } = require('pg');
 
