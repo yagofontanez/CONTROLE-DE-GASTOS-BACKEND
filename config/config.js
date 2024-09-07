11092836868c04c1410c283285d9174f8e34834b
yagofontanez <yagofontanez20@icloud.com>
2024-09-07 16:14:49 -0300

    fix: arrumando arquivo index.js pra deploy

--- config/config.js
+++ config/config.js
@@ -8,18 +8,8 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'postgres',
   },
-  // test: {
-  //   username: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME_TEST,
-  //   host: process.env.DB_HOST,
-  //   dialect: 'postgres',
-  // },
-  // production: {
-  //   username: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME_PROD,
-  //   host: process.env.DB_HOST,
-  //   dialect: 'postgres',
-  // },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  }
 };
