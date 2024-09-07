ec364f21b59ffc90a0abffbecbd07ee687a52a6b
yagofontanez <yagofontanez20@icloud.com>
2024-09-07 18:04:35 -0300

    feat: liberando cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
 app.use(routes);
+app.use(cors);
 
 const { Pool } = require('pg');
 
